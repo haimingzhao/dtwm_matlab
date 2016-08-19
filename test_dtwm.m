d4f6db940fe2151697628b1742ee8c040fb58b74
Haiming Zhao <u1590812@warwick.ac.uk>
2016-08-19 23:50:57 +0100

    imporved plotting in cost matrix

--- test_dtwm.m
+++ test_dtwm.m
@@ -3,10 +3,10 @@ function [d1, d2, L, R, P, OP] = test_dtwm(a, b, t, o, w)
 d1=distance(a,b);
 
 figure; 
-subplot(5,5,[1,6,11,16]); plot(a); axis tight; view(270,90)
-subplot(5,5,22:25); plot(b);
+subplot(5,6,[1,7,13,19]); plot(a); axis tight; view(270,90)
+subplot(5,6,26:29); plot(b); axis tight;
 
-subplot(5,5,[2:5, 7:10, 12:15, 17:20 ]);
+subplot(5,6,[2:6, 8:12, 14:18, 20:24 ]);
 % pcolor(flipud(d3));
 imagesc(flipud(d1(2:end,2:end)));
 colorbar();
